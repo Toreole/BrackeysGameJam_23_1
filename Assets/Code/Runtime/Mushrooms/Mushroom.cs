9623c2445b3db89df3b417eefab810092333ed2b
Toreole <toreole.official@gmail.com>
2023-02-13 00:18:04 +0100

    Mushroom Textures and "Lighting"

--- Assets/Code/Runtime/Mushrooms/Mushroom.cs
+++ Assets/Code/Runtime/Mushrooms/Mushroom.cs
@@ -11,12 +11,14 @@ public class Mushroom : MonoBehaviour
     private SpriteRenderer stemRenderer, capRenderer;
 
     [SerializeField]
-    private float growthRate = 0.05f;
+    private float growthTime = 16f;
+    [SerializeField]
+    private AnimationCurve growthCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
 
     [SerializeField]
     private float maxSize = 1.2f;
 
-    private float size = 0f;
+    private float growthPercentage = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -30,9 +32,9 @@ public class Mushroom : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        size += growthRate * Time.deltaTime;
-        size = Mathf.Min(size, maxSize);
-
+        growthPercentage += Time.deltaTime / growthTime;
+        growthPercentage = Mathf.Min(1, growthPercentage);
+        float size = maxSize * growthCurve.Evaluate(growthPercentage);
         shroomTransform.localScale = new Vector3(size, size, size);
     }
 }
---
+++ Assets/Code/Runtime/PostLighting.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class PostLighting : MonoBehaviour
+{
+    [SerializeField]
+    private Material material;
+    [SerializeField]
+    private LayerMask cullingMask;
+
+    private RenderTexture renderTex;
+    private int width, height;
+
+    Camera otherCam;
+
+    private void Awake()
+    {
+        if (material == null)
+        {
+            Destroy(this);
+            return;
+        }
+
+        width = Screen.width;
+        height = Screen.height;
+
+        renderTex = new RenderTexture(width, height, 0, RenderTextureFormat.ARGB32);
+        material.SetTexture("_RenderTex", renderTex);
+
+        Camera selfCam = gameObject.GetComponent<Camera>();
+
+        GameObject go = new GameObject();
+        go.transform.SetParent(this.transform);
+        go.transform.localPosition = Vector3.zero;
+        
+        otherCam = go.AddComponent<Camera>();
+        otherCam.cullingMask = cullingMask;
+        otherCam.orthographic = selfCam.orthographic;
+        otherCam.orthographicSize = selfCam.orthographicSize;
+        otherCam.targetTexture = renderTex;
+    }
+
+    private void Update()
+    {
+        int w = Screen.width;
+        int h = Screen.height;
+        if(w != width || h != height)
+        {
+            width = w; height = h;
+            var oldTex = renderTex;
+            renderTex = new RenderTexture(width, height, 0, RenderTextureFormat.ARGB32);
+            otherCam.targetTexture = renderTex;
+            oldTex.DiscardContents();
+            Destroy(oldTex);
+            material.SetTexture("_RenderTex", renderTex);
+        }
+        
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, material);
+    }
+}
