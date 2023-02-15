5877a080ffe3062c37de8c8b70c3c3964abaf79d
Toreole <toreole.official@gmail.com>
2023-02-16 00:16:38 +0100

    Camera Panning

--- Assets/Code/Runtime/GameMouseInteract.cs
+++ Assets/Code/Runtime/GameMouseInteract.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,11 +8,41 @@ public class GameMouseInteract : MonoBehaviour
     Collider2D[] buffer = new Collider2D[10];
     [SerializeField]
     private new Camera camera;
+    [SerializeField]
+    private float minX, maxX;
+    [SerializeField]
+    private float panSpeed = 4f;
+    [SerializeField]
+    private float cornerPadding = 0.1f;
+    [SerializeField]
+    private float edgeThickness = 0.07f;
 
     // Update is called once per frame
     void Update()
     {
         HandleClickInteract();
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 screenSize = new Vector2(Screen.width, Screen.height);
+        float cornerOffset = screenSize.y * cornerPadding;
+        float edgeWidth = edgeThickness * screenSize.x;
+
+        Vector2 mousePosition = Input.mousePosition;
+
+        if (mousePosition.y > cornerOffset && mousePosition.y < screenSize.y - cornerOffset)
+            if (mousePosition.x < edgeWidth)
+                Move(-panSpeed);
+            else if (mousePosition.x > screenSize.x - edgeWidth)
+                Move(panSpeed);
+        void Move(float speed)
+        {
+            Vector3 pos = camera.transform.position;
+            pos.x = Mathf.Clamp(pos.x + speed * Time.deltaTime, minX, maxX);
+            camera.transform.position = pos;
+        }
     }
 
     private void HandleClickInteract()
